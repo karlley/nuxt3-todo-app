5f3685b5a992b9fb093251c51a5b947219ae93a2
karlley <karlley.jp@gmail.com>
2024-07-17 14:10:34 +0900

    ベタ書きで一覧表示を追加

--- app.vue
+++ app.vue
@@ -1,6 +1,53 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="body">
+    <h1>Todo App</h1>
+    <div class="card">
+      <ul>
+        <li>Status: Pending</li>
+        <li>Title: タスク1</li>
+        <li>Created At: 2024/7/17</li>
+      </ul>
+    </div>
+    <div class="card">
+      <ul>
+        <li>Status: Working</li>
+        <li>Title: タスク2</li>
+        <li>Created At: 2024/7/18</li>
+      </ul>
+    </div>
+    <div class="card">
+      <ul>
+        <li>Status: Completed</li>
+        <li>Title: タスク3</li>
+        <li>Created At: 2024/7/19</li>
+      </ul>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+.body {
+  display: flex;
+  flex-direction: column; /* 縦に並べる */
+  //justify-content: center; /* 縦方向の中央揃え */
+  align-items: center; /* 横方向の中央揃え */
+}
+
+.card {
+  border: 1px solid black;
+  width: 250px;
+  margin-bottom: 20px;
+}
+
+.card ul {
+  list-style-type: none;
+}
+
+.card li {
+  margin-top: 5px;
+  margin-bottom: 5px;
+}
+</style>
