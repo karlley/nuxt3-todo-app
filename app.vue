4b3cdf8d839d081204811628784f170686e94ddb
karlley <karlley.jp@gmail.com>
2024-07-17 15:48:42 +0900

    localStrageを使用してタスクの永続化

--- app.vue
+++ app.vue
@@ -12,34 +12,44 @@
 </template>
 
 <script setup lang="ts">
-const tasks = [
-  {
-    id: 1,
-    status: 'pending',
-    title: 'タスク1',
-    created_at: new Date('2024-7-17')
-  },
-  {
-    id: 2,
-    status: 'working',
-    title: 'タスク2',
-    created_at: new Date('2024-7-18')
-  },
-  {
-    id: 1,
-    status: 'completed',
-    title: 'タスク3',
-    created_at: new Date('2024-7-19')
+import { ref, onMounted } from 'vue';
+
+const tasks = ref([]);
+
+onMounted(() => {
+  const storedTasks = localStorage.getItem('tasks');
+  if (storedTasks) {
+    tasks.value = JSON.parse(storedTasks);
+  } else {
+    tasks.value = [
+      {
+        id: 1,
+        status: 'pending',
+        title: 'タスク1',
+        created_at: new Date('2024-7-17')
+      },
+      {
+        id: 2,
+        status: 'working',
+        title: 'タスク2',
+        created_at: new Date('2024-7-18')
+      },
+      {
+        id: 1,
+        status: 'completed',
+        title: 'タスク3',
+        created_at: new Date('2024-7-19')
+      }
+    ]
   }
-]
+})
 </script>
 
 <style>
 .body {
   display: flex;
-  flex-direction: column; /* 縦に並べる */
-  //justify-content: center; /* 縦方向の中央揃え */
-  align-items: center; /* 横方向の中央揃え */
+  flex-direction: column;
+  align-items: center;
 }
 
 .card {
