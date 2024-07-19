2bd296fb3a60345a74fd44ef679b1df8c5e6122b
karlley <karlley.jp@gmail.com>
2024-07-19 12:23:12 +0900

    作成日を一旦削除

--- components/TodoList.vue
+++ components/TodoList.vue
@@ -4,7 +4,6 @@
       <ul>
         <li>Status: {{ task.status }}</li>
         <li>Title: {{ task.title }}</li>
-        <li>Created At: {{ task.created_at.toLocaleDateString() }}</li>
       </ul>
     </div>
   </div>
@@ -18,26 +17,18 @@ const tasks = useStorage('tasks', [
     id: 1,
     status: 'pending',
     title: 'タスク1',
-    created_at: new Date('2024-7-17')
   },
   {
     id: 2,
     status: 'working',
     title: 'タスク2',
-    created_at: new Date('2024-7-18')
   },
   {
     id: 3,
     status: 'completed',
     title: 'タスク3',
-    created_at: new Date('2024-7-19')
   }
 ]);
-
-//文字列から日付オブジェクトに変換
-tasks.value = tasks.value.map(task => ({
-  ...task, created_at: new Date(task.created_at)
-}));
 </script>
 
 <style>
